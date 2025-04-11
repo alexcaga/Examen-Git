0ec8c9a0b17217ef82a5d1c232de254c0937fb1a
AlexFPCadiz <alex-guillermo.carpio.garcia.alu@iesfernandoaguilar.es>
2025-04-11 12:51:51 +0200

    Primer Commit

---
+++ Artista.java
@@ -0,0 +1,24 @@
+package evf.javadoc.original;
+// Nombre: Álex-Guillermo Carpio García
+public class Artista {
+    String nombreArtista;
+    public Artista(String nombreArtista) {
+        this.nombreArtista = nombreArtista;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "nombreArtista='" + nombreArtista + '\'' +
+                '}';
+    }
+
+    public String getNombreArtista() {
+        return nombreArtista;
+	//Prueba fetch
+	//Prueba fetch 2
+    }
+    
+}
+
+
